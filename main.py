4052926550af7782c97d7866a1a3638b132023a9
sadam <sadamccr@gmail.com>
2022-10-09 21:01:33 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+name = 'Abu Bakar'
+
+print(name)
+
+salam ='''
+Asalam
+Alaikum
+'''
+
+print(salam)
+
+## Comma, Appostrophe
+
+single_quote_str = 'He said, "He\'s fine"'
+
+print(single_quote_str)
+
+## Embedding value
+
+myscore = 100
+
+message = 'my score %s points'
+print(message % myscore)
+
+# another example
+
+nums = 'what did the number %s says to %s? Nice belt!!'
+
+print(nums%(0,8))
+
+# Multiply String
+
+str = "a"
+
+print(str*5)
+
+# embed space and string
+
+space = ' ' * 10
+name = 'my name is %s sadam'
+print(name % space)
+
+# Jupyter Linked to Github
+
+print("testing connection")
+
+# List
+
+fruit_list = ['mangoes', 'bananas', 'graphes']
+
+print(fruit_list)
+
+print(fruit_list[0])
+
+print((fruit_list[0:2]))
+
+# list holds different data types
+
+_list = ['list','can', 'hold','integers', 'i.e', 1,2,3,4, 'and', 'strings', 'together']
+
+print(_list)
+
+# list in list
+
+num = [1,2,3,4,5]
+
+str = ['asd','zxc']
+
+mylist =[num,str]
+
+
+print(mylist)
+
+# List Appending
+
+str.append('xcv')
+
+print(mylist)
+
+# delete from list
+
+del str[0]
+
+print(mylist)
+
